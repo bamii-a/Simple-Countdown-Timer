d723af9223919d34e67093d5ab6b255bba3e41a7
Bamidele <bamidele@Bamms-2.fritz.box>
2021-03-25 05:15:08 +0100

    update countdown

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-## Simple-Countdown-Timer
+## Overview
 
-This is a simple countdown timer using Vanilla JavaScript
+### Simple-Countdown-Timer to New Year 2022 [Challange gotten from Frontend Mentor]
+This is a simple countdown timer built using html/css and Vanilla JavaScript.
+Shows how many days left until the new year 2022.
+
+Users should be able to:
+- See hover states for all interactive elements on the page.
+- See a live countdown timer that ticks down every second.
+
+
+### Built with
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Desktoop-first workflow
+
+
+### Author
+- Website - [Bamii](https://github.com/bamii-a)
+- Frontend Mentor - [@bamii-a](https://www.frontendmentor.io/profile/bamii-a)
+
+
+### What I learned
+This project increased my knowledge of JavaScript, improved my prb=oblem solving and a better understanding of how time works in milli seconds.
---
+++ app.js
@@ -0,0 +1,79 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+//selectors
+const countdown = document.querySelector('.countdown');
+const counts = document.querySelectorAll('.countdown-format h4');
+
+//get dates in milliseconds
+const newYear = new Date(2022, 0, 01);
+
+
+
+
+//function
+function newYearsDay() {
+    const newYearInMilliSeconds = new Date(2022, 0, 01).getTime()
+    const todayInMilliSeconds = new Date().getTime();
+    const currentTimeRemaining = newYearInMilliSeconds - todayInMilliSeconds;
+
+    //get time values in milliseconds
+    const oneDay = 3600 * 24 * 1000;        //3600seconds(1hour)*24hours
+    const oneHour = 3600 * 1000;
+    const oneMin = 60 * 1000;               //all values are multiplied by 1000 to get values in Milliseconds
+    
+    //get time values Remaining in days, hours, minutes and seconds
+    let daysValue = currentTimeRemaining / oneDay;
+    daysValue = Math.floor(daysValue);
+
+    let hourValue = (currentTimeRemaining % oneDay) / oneHour;
+    hourValue = Math.floor(hourValue);
+
+    let minValue = (currentTimeRemaining % oneHour) / oneMin;
+    minValue = Math.floor(minValue);
+    
+    let secValue = (currentTimeRemaining % oneMin) / 1000;
+    secValue = Math.floor(secValue);
+
+    // console.log(daysValue, hourValue, minValue, secValue);
+
+    const timeValues = [daysValue, hourValue, minValue, secValue];
+    //time format "01.01.02"
+    function timeFormat(element) {
+        if (element < 10) {
+            return element = `0${element}`
+        }
+        else {
+            return element;
+        }
+    }
+    //iterate over the count values
+    counts.forEach((element, index) => {
+        element.innerHTML = timeFormat(timeValues[index]);
+    });
+}
+
+//Timer
+let countdownTimer = setInterval(newYearsDay, 1000);
+newYearsDay();
